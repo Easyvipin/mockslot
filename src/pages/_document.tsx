c32418e98428b9ed0d1aa13b6930eb2c41471d37
easyvipin <vipinchandra555@gmail.com>
2023-07-01 19:46:55 +0530

    feat: bannder designed

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Amiko:wght@400;600;700&family=Chivo:wght@100;400&family=Poppins:wght@100;400;700;800&display=swap"
+        rel="stylesheet"
+      />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
